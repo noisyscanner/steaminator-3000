bdc1133a2c96f9a8db76acffc21414a87b7ab180
Brad Reed 😎 <brad@bradreed.co.uk>
2022-05-27 23:02:14 +0100

    arduino: parse recipe and turn pumps on/off in parallel

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,22 @@
+// 1.5-2L per minute, 1.75 avg, 1750ml
+const float FLOW = 1.75 / 60;
+const float MS_PER_ML = 1750 / 60;
+
+// TODO: "pull through" mechanism, where we can turn on/off at will to get liquid to end of pipe
+// TODO: allow querying arduino for currently installed ingredients
+
 struct PinRecipe {
   byte pin;
-  byte ml;
+  unsigned long ms;
 };
 
 struct PinRecipe recipe[8];
 
 const unsigned int MAX_MESSAGE_LENGTH = 12;
-int len = 0;
+unsigned int len = 0;
+byte ingredientIndex = 0;
+unsigned long dispenseStartTime = 0;
+unsigned long longestDispenseTime = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -21,63 +31,77 @@ void setup() {
 }
 
 void loop() {
- while (Serial.available() > 0)
- {
-   static char message[MAX_MESSAGE_LENGTH];
-   static unsigned int message_pos = 0;
+  while (Serial.available() > 0) {
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos = 0;
+    char inByte = Serial.read();
+    len += 1;
 
-   //Read the next available byte in the serial receive buffer
-   char inByte = Serial.read();
-   len += 1;
+    if (inByte != '\n' && message_pos < MAX_MESSAGE_LENGTH - 1) {
+      message[message_pos] = inByte;
+      message_pos++;
+    } else {
+      message[message_pos] = '\0';
+      String str = String(message);
 
-   //Message coming in (check not terminating character) and guard for over message size
-   if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
-   {
-     //Add the incoming byte to our message
-     message[message_pos] = inByte;
-     message_pos++;
-   }
-   //Full message received...
-   else
-   {
-     //Add null character to string
-     message[message_pos] = '\0';
+      if (str == "go") {
+        dispense();
+        reset();
+      } else {
+        byte pin = message[0] - '0';
+        int ml = str.substring(2).toInt();
+        prepare(pin, ml);
+      }
 
-     String str = String(message);
-
-     //Print the message (or do other things)
-//     Serial.println(str);
-//     Serial.println(len - 1);
-
-     byte pin = message[0] - '0';
-     int ml = str.substring(2).toInt();
-     dispense(pin, ml);
-
-     //Reset for the next message
-     message_pos = 0;
-     len = 0;
-   }
- }
+      // Reset for the next message
+      message_pos = 0;
+      len = 0;
+    }
+  }
 }
 
-// TODO: message format [pin][ml] space separated, to avoid dispensing before receiving whole recipe
-// TODO: "pull through" mechanism, where we can turn on/off at will to get liquid to end of pipe
-// TODO: pump at once, set timeouts for turning pumps off
+void reset() {
+  ingredientIndex = 0;
+  longestDispenseTime = 0;
+}
 
-// 1.5-2L per minute, 1.75 avg, 1750ml
-const float FLOW = 1.75 / 60;
-const float MS_PER_ML = 1750 / 60;
+void prepare(byte pin, int ml) {
+  unsigned long ms = MS_PER_ML * ml;
+  struct PinRecipe ingredient = { pin, ms };
+  recipe[ingredientIndex] = ingredient;
+  ingredientIndex++;
 
-void dispense(byte pin, int ml) {
-  Serial.print("Pin ");
-  Serial.println(pin);
-  Serial.println(MS_PER_ML * ml);
-//  Serial.print(ml);
-//  Serial.println(" ml");
+  if (ms > longestDispenseTime) {
+    longestDispenseTime = ms;
+  }
+}
 
-  digitalWrite(pin, HIGH);
-  Serial.println("On");
-  delay(MS_PER_ML * ml);
-  digitalWrite(pin, LOW);
-  Serial.println("Off");
+void dispense() {
+  unsigned long dispenseStartTime = millis();
+  unsigned long endTime = dispenseStartTime + longestDispenseTime;
+  Serial.print("Ends at ");
+  Serial.println(endTime);
+
+  for (byte i = 0; i < ingredientIndex; i++) {
+    byte pin = recipe[i].pin;
+    Serial.print("On ");
+    Serial.println(pin);
+
+    digitalWrite(pin, HIGH);
+  }
+
+  unsigned long mil;
+  while ((mil = millis()) <= endTime) {
+    for (byte i = 0; i < ingredientIndex; i++) {
+      unsigned long endTimeForPin = dispenseStartTime + recipe[i].ms;
+      if (mil >= endTimeForPin) {
+        byte pin = recipe[i].pin;
+        Serial.print("Off ");
+        Serial.println(pin);
+        digitalWrite(pin, LOW);
+      }
+    }
+
+//    delay(10);
+  }
 }
